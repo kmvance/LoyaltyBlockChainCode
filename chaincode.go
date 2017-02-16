70bade007435d0e3cbc82ed05c5fe844647232a0
gscdist <scheng@us.ibm.com>
2017-02-16 11:09:18 -0600

    Add files via upload

--- chaincode.go
+++ chaincode.go
@@ -459,7 +459,6 @@ func retailContract(tx Transaction, stub shim.ChaincodeStubInterface) float64 {
 	
 	var pointsToTransfer float64
 	pointsToTransfer = tx.Amount
-    pointsToTransfer = pointsToTransfer * 0.8
 	if (tx.Date.After(contract.StartDate) && tx.Date.Before(contract.EndDate)) {
 	     pointsToTransfer = pointsToTransfer * 0.8
 	}
